d7304b176abde1390e25878508116e939a0be6af
maina3joseph9ngugi <110392941+maina3joseph9ngugi@users.noreply.github.com>
2022-10-11 14:37:54 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div>About
+    <div id="about">
+    <h2>About Me</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit nisi, voluptatibus accusamus quia repellat.</p>
+    <img src={image} alt="I made this"/>
+    </div>
+  </div>);
 }
 
 export default About;
